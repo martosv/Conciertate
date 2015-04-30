24dbd13186ee9ee0171468490aac5bde1cda32fb
jpmarinjimenez <jpmarinjimenez@gmail.com>
2015-04-30 21:38:34 +0200

    like button created

--- conciertate/app/assets/javascripts/concerts.js
+++ conciertate/app/assets/javascripts/concerts.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+window.onload = function() {
+    button = document.getElementById('like-button');
+   button.addEventListener('click', likeConcert, false);
+};
+
+function likeConcert () {
+    button.classList.add('liked');
+    button.innerHTML = "Liked!";
+    document.querySelector('h1').style.color = "red";
+}
--- conciertate/db/schema.rb
+++ conciertate/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429153423) do
+ActiveRecord::Schema.define(version: 20150430190939) do
 
   create_table "concerts", force: :cascade do |t|
     t.string   "band"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20150429153423) do
     t.string   "photo"
     t.string   "description"
     t.string   "tickets"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "likes",       default: 0
   end
 
 end
