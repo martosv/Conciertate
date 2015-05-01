826ebe6b6fd0a0877af90a47e7d20c7957282792
martosv <virginia.martos.muoz@gmail.com>
2015-05-01 20:56:07 +0200

    todo juntado-segunda prueba

--- conciertate/app/controllers/concerts_controller.rb
+++ conciertate/app/controllers/concerts_controller.rb
@@ -46,13 +46,13 @@ class ConcertsController < ApplicationController
         render :show
     end
 
-    def top10
-        likes_concert = Hash.new
-        likes_concert[@concert.likes] = @concert.band
-        likes_concert.sort.reverse
-        length = likes_concert.length
-        better_concerts = likes_concert.delete_if{|key,value| key.length > 10}
-    end
+    #def top10
+     #   likes_concert = Hash.new
+      #  likes_concert[@concert.likes] = @concert.band
+       # likes_concert.sort.reverse
+        #length = likes_concert.length
+        #better_concerts = likes_concert.delete_if{|key,value| key.length > 10}
+    #end
 
     private # para aceptar solo los parametros que nos interesa o se quiere controlar
     def concert_params
